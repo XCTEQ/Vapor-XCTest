a3ec606730c078af1bd658af3f4c06195b1dfbd3
Shashikant86 <shashikant.jagtap@icloud.com>
2018-03-02 12:52:47 +0000

    Demo app for Vapor XCUITest Demo

---
+++ Vapor-XCTestUITests/LocationCheckWithVaporStub.swift
@@ -0,0 +1,35 @@
+//
+//  LocationCheckWithVaporStub.swift
+//  Vapor-XCTestUITests
+//
+//  Created by Shashikant Jagtap on 02/03/2018.
+//  Copyright © 2018 Shashikant Jagtap. All rights reserved.
+//
+
+import XCTest
+
+class LocationCheckWithVaporStub: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testLocationWithVaporStub() {
+        let app = XCUIApplication()
+        app.launchEnvironment = ["BASEURL" : "http://localhost:8080"]
+        app.launch()
+        let gtusernametextfieldTextField = app.textFields["gtUserNameTextField"]
+        gtusernametextfieldTextField.tap()
+        gtusernametextfieldTextField.typeText("Shashikant86")
+        app.buttons["ShowButton"].tap()
+        let existsPredicate = NSPredicate(format: "exists == true")
+        expectation(for: existsPredicate, evaluatedWith: app.staticTexts["London"], handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+        XCTAssert(app.staticTexts["London"].exists)
+    }
+}
